3dd2f9a6f501e2d959f9adb4f3f9700b8bcda36c
otegami <a.s.takuya1026@gmail.com>
2020-12-01 21:29:47 +0900

    implement components with connect function

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 
 import './index.css';
 import reducer from './reducers';
-import App from './components/App.js';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
 const store = createStore(reducer)
